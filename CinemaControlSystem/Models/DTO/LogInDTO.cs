f0738910c9b4cd502659c69c7ce43103f20c3b24
yagizerdem <2yagiz004@gmail.com>
2025-01-14 18:56:52 +0300

    auth ui

---
+++ CinemaControlSystem/Models/DTO/LogInDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaControlSystem.Models.DTO
+{
+    public class LogInDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ CinemaControlSystem/Models/DTO/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaControlSystem.Models.DTO
+{
+    public class RegisterDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
